584727dfe0e555efdf263df1f3aab8fa41f48bf7
hasanhabsyi2011 <71365610+hasanhabsyi2011@users.noreply.github.com>
2020-09-28 16:38:16 +0700

    Add files via upload

---
+++ precache-manifest.4ebdd66b5104779f6cb39dca31bab54d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c7e2f0a1e38d94d5f4acae01bd778e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3c0b9b4abe6be7c4fec",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fb6b885b244a9f13a20d",
+    "url": "/static/js/2.d69876ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d69876ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3c0b9b4abe6be7c4fec",
+    "url": "/static/js/main.16814ef4.chunk.js"
+  },
+  {
+    "revision": "7446b63276108d351200",
+    "url": "/static/js/runtime-main.ad615bf2.js"
+  }
+]);
